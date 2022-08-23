cef74c59fac838374777bdb8658a72ac29f5ff80
bigeppo <carlog91cast@gmail.com>
2022-08-23 15:34:42 +0200

    starting repo

---
+++ index.php
@@ -0,0 +1,34 @@
+<!-- <?php
+        // $matchMilano = array(
+        //     "Milano" => 21,
+        //     "La Spezia" => 33,
+        // );
+        // echo "$matchMilano";
+
+        // $matchRoma = array(
+        //     "Roma" => 15,
+        //     "Vieste" => 48,
+        // );
+        // $matchCagliari = array(
+        //     "Cagliari" => 84,
+        //     "Salerno" => 25,
+        // )
+        ?> -->
+
+<!-- snack 4 -->
+
+<!-- <?php
+$random = array();
+for ($i = 0; $i <= 15; $i++) {
+    $random[] = rand(0, 15) . "<br />";
+}
+$controllo = true;
+if (!in_array($i, $random)) {
+    $random[] = $i;
+    $controllo = false;
+}
+print_r($random);
+?> -->
+
+<!-- snack 5 -->
+
