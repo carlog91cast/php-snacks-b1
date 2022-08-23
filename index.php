5968daa0f84708a3f9747cfdf8317778eedd567c
bigeppo <carlog91cast@gmail.com>
2022-08-23 17:07:16 +0200

    snack 2: end

--- index.php
+++ index.php
@@ -55,18 +55,24 @@
     <h2>Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione)
         che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero.
         Se tutto è ok stampare "Accesso riuscito", altrimenti "Accesso negato"</h2>
-        
+
     <form action="index.php" method="GET">
         <input type="text" name="name" placeholder="name">
-        <input type="email" name="email" placeholder="email">
+        <input type="email" name="mail" placeholder="email">
         <input type="text" name="age" placeholder="age">
         <button type="submit">submit</button>
     </form>
 
     <?php
-    $name = $_GET['name'];
-    $mail = $_GET['mail'];
-    $age = $_GET['age'];
+        $name = $_GET['name'];
+        $mail = $_GET['mail'];
+        $age = $_GET['age'];
+
+        if ((strlen($name) > 3) && (strpos($mail, '@') > 0 && strpos($mail, '.') > 3) && (is_numeric($age))){
+            echo "accesso riuscito";
+        } else {
+            echo "accesso negato";
+        }
     ?>
 </body>
 
